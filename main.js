0db90c4b51813a594f6675fb5b06c4470cbd619d
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2023-11-07 10:29:48 +0100

    Affichage du tableau 1

--- main.js
+++ main.js
@@ -1,182 +1,156 @@
-// // Créez un élément canvas pour le graphique
-// var canvas = document.createElement('canvas');
-// canvas.id = 'monGraphique';
-
-// // Récupérez le titre principal (h1) par son ID
-// var titrePrincipal = document.querySelector('#firstHeading');
-
-// // Insérez l'élément canvas après le titre principal
-// titrePrincipal.parentNode.insertBefore(canvas, titrePrincipal.nextSibling);
-
-// // Obtenez le contexte 2D du canvas
-// var ctx = canvas.getContext('2d');
-
-// // Définissez les données pour votre graphique
-// var data = {
-//   labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai'],
-//   datasets: [
-//     {
-//       label: 'Ventes mensuelles',
-//       data: [50, 65, 80, 72, 90],
-//       backgroundColor: 'rgba(75, 192, 192, 0.2)',
-//       borderColor: 'rgba(75, 192, 192, 1)',
-//       borderWidth: 1,
-//     },
-//   ],
-// };
-
-// // Créez une instance de Chart.js avec les données et les options
-// var myChart = new Chart(ctx, {
-//   type: 'bar',
-//   data: data,
-//   options: {
-//     responsive: true,
-//     maintainAspectRatio: false,
-//   },
-// });
-
-
-/*
-Insertion de la balise  canvas en dessous du h1
-*/ 
-
-// const table2 = document.querySelector('#Homicides');
-// const newP = document.createElement('p');
-// newP.textContent = 'BONJOUR A TOUS';
-// let canvasTable2 = document.createElement('canvas');
-
-/*
-La méthode insertAdjacentHTML attend une chaîne de texte HTML en tant que deuxième argument, mais vous lui avez passé un élément DOM (newP). Pour ajouter l'élément newP en tant que paragraphe en dessous de l'élément h3 avec l'id "#Homicides", vous pouvez utiliser la méthode insertAdjacentElement 
-*/
-
-// table2.insertAdjacentElement("beforeend",newP );
-
-// const table2 = document.querySelector('#Homicides');
-// const canvasTable2 = document.createElement('canvas');
-
-// const barChart = new Chart(canvasTable2, {
-// type: "bar",
-// data: ['Bruxel', 'Leuven', 'Aarschoot', 'Diest'],
-// datasets: [{
-// data: [240, 120, 140, 110],
-
-// }]
-// })
-// table2.insertAdjacentElement("beforeend", barChart)
+// /* Insertion du graphique au-dessus du tableau 1 */
+const table1 = document.querySelector('#table1');
+const parentTables = document.getElementById('mw-content-text');
+const canvasTable1 = document.createElement('canvas');
+canvasTable1.setAttribute("id", "idCanvas1");
+parentTables.insertBefore(canvasTable1, table1);
 
 
 
-// const h4Element2 = document.querySelector('#Homicides');
-// const canvasTable2 = document.createElement('canvas');
-
-// const barChart = new Chart(canvasTable2, {
-//   type: "bar",
-//   data: {
-//     labels: ['Bruxel', 'Leuven', 'Aarschoot', 'Diest'],
-//     datasets: [{
-//       label: 'Nombre de Homicides',
-//       data: [240, 120, 140, 110],
-//       backgroundColor: 'rgba(75, 192, 192, 0.2)',
-//       borderColor: 'rgba(75, 192, 192, 1)',
-//       borderWidth: 1
-//     }]
-//   }
-// });
-
-// h4Element2.insertAdjacentElement("afterend", canvasTable2);
-
-
-
-
-
-// const h4Element2 = document.querySelector('#Homicides');
-// const canvasTable2 = document.createElement('canvas');
-
-// const barChart = new Chart(canvasTable2, {
-//   type: 'bar',
-//   data: {
-//     labels: data.labels,
-//     datasets: [
-//       {
-//         label: '2007-09',
-//         data: data.data2007,
-//         backgroundColor: 'rgba(75, 192, 192, 0.2)'
-//       },
-//       {
-//         label: '2010-12',
-//         data: data.data2010,
-//         backgroundColor: 'rgba(255, 99, 132, 0.2)'
-//       }
-//     ]
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   }
-// });
-
-
-// h4Element2.insertAdjacentElement("afterend", canvasTable2);
-
+/*  Insertion du graphe au dessus du tableau 2 start*/ 
 
 const table2 = document.querySelector('#table2');
 const canvasTable2 = document.createElement('canvas');
-
 canvasTable2.setAttribute("id", "idCanvas2");
-const parentTables = document.getElementById('mw-content-text');
-
 parentTables.insertBefore(canvasTable2, table2);
 
 
-// Extraire les données du tableau HTML
 
-let countries = [];
-let years = [];
-let dataDigits = [];
+const tableFunction1 = ()=>{
 
-const topTable2 = table2.rows[0];
-for(let i = 2; i< topTable2.cells.length; i++){
-  years.push(topTable2.cells[i].textContent);
+    // // Extraire les données du tableau HTML (même code que pour table2)
+  let countries1 = [];
+  let years1 = [];
+  let dataDigits1 = [];
 
-}
-
-for(let i = 1; i< table2.rows.length; i++){
-  const row = table2.rows[i];
-  countries.push(row.cells[1].textContent);
-  
-  const dataCell = [];
-  for( let j = 2; j< row.cells.length; j++){
-    dataCell.push(parseFloat(row.cells[j].textContent.replace(",", ".")));
+  const topTable1 = table1.rows[1];
+  for(let x = 2; x < topTable1.cells.length; x++) {
+    years1.push(topTable1.cells[x].textContent);
   }
-  dataDigits.push(dataCell);
-  
-}
 
-tableData = { countries, years, dataDigits };
-const ctx = document.getElementById("idCanvas2");
+  console.log(topTable1);
+  for(let x = 2; x < table1.rows.length; x++) {
+    const row = table1.rows[x];
+    countries1.push(row.cells[1].textContent);
+    
+    const dataCell = [];
+    for(let y = 2; y < row.cells.length; y++) {
+      dataCell.push(parseFloat(row.cells[y].textContent.replace(",", ".")));
+    }
+    dataDigits1.push(dataCell);
+  }
+  tableData = { countries1, years1, dataDigits1 };
+  const ctx1 = document.getElementById("idCanvas1");
+  console.log(tableData); 
 
-// Créer le graphique Chart.js
-const barChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: tableData.countries,
-    datasets: tableData.years.map((year, index) => ({
-      label: year,
-      data: tableData.dataDigits.map(digit => digit[index]),
-      borderWidth: 1
-    }))
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+  // Créer le graphique Chart.js pour table1
+  const barChart1 = new Chart(ctx1, {
+    type: 'bar',
+    data: {
+      labels: countries1,
+      datasets: years1.map((year, index) => ({
+        label: year,
+        data: dataDigits1.map(digit => digit[index]),
+        borderWidth: 1
+      }))
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
+  });
+
+}
+
+
+
+
+const tableFunction2 = ()=>{
+  // Extraire les données du tableau HTML
+
+  let countries = [];
+  let years = [];
+  let dataDigits = [];
+
+  const topTable2 = table2.rows[0];
+
+
+
+  for(let i = 2; i< topTable2.cells.length; i++){
+    years.push(topTable2.cells[i].textContent);
+
   }
-});
+
+
+
+  for(let i = 1; i< table2.rows.length; i++){
+    const row = table2.rows[i];
+    countries.push(row.cells[1].textContent);
+    
+    const dataCell = [];
+    for( let j = 2; j< row.cells.length; j++){
+      dataCell.push(parseFloat(row.cells[j].textContent.replace(",", ".")));
+    }
+    dataDigits.push(dataCell);
+    
+  }
+
+  tableData = { countries, years, dataDigits };
+  const ctx = document.getElementById("idCanvas2");
+
+  // Créer le graphique Chart.js
+  const barChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: tableData.countries,
+      datasets: tableData.years.map((year, index) => ({
+        label: year,
+        data: tableData.dataDigits.map(digit => digit[index]),
+        borderWidth: 1
+      }))
+    },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+}
+tableFunction1();
+tableFunction2();
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+// console.log(canvasTable1);
+
+
+
+/*  Insertion du graphe au dessus du tableau 1 End*/ 
+
+
 
 // Insérer le graphique sous la balise h4
 
